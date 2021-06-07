523d89b00715215390b99d34880cf0ec2751f497
Camilo <cavs1010@gmail.com>
2021-06-07 18:19:06 +1000

    commit

--- Mixed Messages/mixedMessages.js
+++ Mixed Messages/mixedMessages.js
@@ -45,7 +45,32 @@ const dataBaseMessages = {
                 "Go to a pet shop and ask for a cow",
                 "In an elevator with many people in it, say “you may be wondering why I’ve gathered you here today”",
                 "Make loud groans in a public bathroom then drop a cantaloupe in the toilet and sigh in relief",
-                "Drive a tricycle past a cop while drinking a juice pouch screaming “YOU CAN’T CATCH ME”"]
+                "Drive a tricycle past a cop while drinking a juice pouch screaming “YOU CAN’T CATCH ME”"],
+    quoteList:["Love For All, Hatred For None. – Khalifatul Masih III",
+        "Change the world by being yourself. – Amy Poehler",
+        "Every moment is a fresh beginning. – T.S Eliot",
+        "Never regret anything that made you smile. – Mark Twain",
+        "Die with memories, not dreams. – Unknown",
+        "Aspire to inspire before we expire. – Unknown",
+        "Everything you can imagine is real. – Pablo Picasso",
+        "Simplicity is the ultimate sophistication. – Leonardo da Vinci",
+        "Whatever you do, do it well. – Walt Disney",
+        "What we think, we become. – Buddha",
+        "All limitations are self-imposed. – Oliver Wendell Holmes",
+        "Tough times never last but tough people do. – Robert H. Schiuller",
+        "Problems are not stop signs, they are guidelines. – Robert H. Schiuller",
+       " One day the people that don’t even believe in you will tell everyone how they met you. – Johnny Depp",
+        "If I’m gonna tell a real story, I’m gonna start with my name. – Kendrick Lamar"
+        If you tell the truth you don’t have to remember anything. – Mark Twain
+        Have enough courage to start and enough heart to finish. – Jessica N. S. Yourko
+        Hate comes from intimidation, love comes from appreciation. – Tyga
+        I could agree with you but then we’d both be wrong. – Harvey Specter
+        Oh, the things you can find, if you don’t stay behind. – Dr. Seuss
+        Determine your priorities and focus on them. – Eileen McDargh
+        Be so good they can’t ignore you. – Steve Martin
+        Dream as if you’ll live forever, live as if you’ll die today. – James Dean
+        Yesterday you said tomorrow. Just do it. – Nike
+        I don’t need it to be easy, I need it to be worth it. – Lil Wayne]
 }
 
 console.log(dataBaseMessages.weatherList[5]);
